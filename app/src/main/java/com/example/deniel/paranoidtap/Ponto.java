177a827d48fa0d1f2666138cfacee192fd06f001
denieldiniz <denieldiniz@gmail.com>
2015-12-09 13:21:46 -0300

    0.02

---
+++ app/src/main/java/com/example/deniel/paranoidtap/Ponto.java
@@ -0,0 +1,15 @@
+package com.example.deniel.paranoidtap;
+
+/**
+ * Created by deniel on 09/12/15.
+ */
+public class Ponto {
+
+    private Integer x;
+    private Integer y;
+    private Boolean pressed;
+    private Integer imgUnpressed;
+    private Integer imgPressed;
+    //http://pt.androids.help/q79  <<  sobre desenho com material design
+
+}
